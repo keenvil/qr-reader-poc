66c30e3d6eb845d5d84a848b713e69b324ab4838
Esteban Abait <estebanabait@Estebans-MBP.cpe.telecentro.net.ar>
2016-05-05 15:18:54 -0300

    Initial app commit

---
+++ qr-reader-poc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  qr-reader-poc
+//
+//  Created by Esteban Abait on 5/5/16.
+//  Copyright © 2016 my-community. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ qr-reader-pocTests/qr_reader_pocTests.swift
@@ -0,0 +1,36 @@
+//
+//  qr_reader_pocTests.swift
+//  qr-reader-pocTests
+//
+//  Created by Esteban Abait on 5/5/16.
+//  Copyright © 2016 my-community. All rights reserved.
+//
+
+import XCTest
+@testable import qr_reader_poc
+
+class qr_reader_pocTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
